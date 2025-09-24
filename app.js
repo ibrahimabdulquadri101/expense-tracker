5d3bced6103d49ef8e540916ced3bb5d7951833e
ibrahimabdulquadri101 <ibrahimabdulquadri446@gmail.com>
2025-09-24 22:28:13 +0100

    2

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ import errorMiddleware from "./middlewares/error.middleware.js";
 import { expenseRouter } from "./routes/expense.route.js";
 const app = express();
 app.use(express.json());
-app.use(errorMiddleware)
-app.use("/api/v1/auth",AuthRouter)
-app.use("/api/v1/expense",expenseRouter)
+app.use(errorMiddleware);
+app.use("/api/v1/auth", AuthRouter);
+app.use("/api/v1/expense", expenseRouter);
 app.listen(3000, (req, res) => {
   console.log("App up and running");
+  res.json({
+    Message: "Expense Tracker",
+  });
   connectToDatabase();
 });
